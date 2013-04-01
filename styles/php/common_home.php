9e83dc564399b2a7d067898f6dd64e30d6e7d8ea
Diogo Nunes <dialex.nunes@gmail.com>
2013-04-01 12:17:17 +0100

    new colored brand name at navbar

--- styles/php/common_home.php
+++ styles/php/common_home.php
@@ -8,7 +8,7 @@ $navbar_top_home = '
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span> </a>
-                <a class="brand" href="/"><img src="/styles/img/logo-code4pt-draft3-grad-mini.png" width="23px" height="auto" />&nbsp;Code4PT</a>
+                <a class="brand" href="/"><img src="/styles/img/logo-code4pt-draft3-grad-mini.png" width="23px" height="auto" />&nbsp;<span style="color:#00CC00">Code</span><span style="color:yellow">4</span><span style="color:red">PT</span></a>
                 <div class="nav-collapse collapse" style="height: 0px; ">
                     <ul class="nav">
                         <li class="active"><a href="/">Home</a></li>
